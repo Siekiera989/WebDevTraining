d16baf4d31f28bfe92d7bbc10040beef08802a8a
Adam Siekerski <adam.siekerski@aluplast.com.pl>
2020-08-05 11:10:18 +0200

    Coloer game finished

--- JS_Section/ColorGame/script.js
+++ JS_Section/ColorGame/script.js
@@ -1,7 +1,27 @@
 var resetBtn = document.querySelector("#refreshBtn");
-resetBtn.addEventListener('click',refreshColors);
+var easyBtn = document.querySelector("#easyBtn");
+var hardBtn = document.querySelector("#hardBtn");
 
-var colors = generateRandomColors(6);
+var gameMode = 6;
+
+
+resetBtn.addEventListener('click',function(){
+    refreshColors(gameMode);        
+});
+easyBtn.addEventListener('click',function(){
+    gameMode=3;
+    refreshColors(gameMode);
+    hardBtn.classList.remove("selected");
+    easyBtn.classList.add("selected");
+});
+hardBtn.addEventListener('click',function(){
+    gameMode=6;    
+    refreshColors(gameMode);
+    hardBtn.classList.add("selected");
+    easyBtn.classList.remove("selected");
+});
+
+var colors = generateRandomColors(gameMode);
 
 function generateRandomColors(num){
     var arr = [];
@@ -57,13 +77,18 @@ function changeBackground(){
     }
 }
 
-function refreshColors(){
-    colors = generateRandomColors(6);
+function refreshColors(num){
+    document.querySelector("#message").textContent = "";
+    colors = generateRandomColors(num);
     pickedColor = pickColor();
     colorToDisplay.textContent = pickedColor;
     setSquaresColor();
-    document.querySelector("#header").style.backgroundColor = "#232323";
+    if(num===3){
+        hideSquares();
+    }
+    
     resetBtn.textContent = "New color";
+    document.querySelector("#header").style.backgroundColor = "steelblue";
 }
 
 function setSquaresColor(){
@@ -72,4 +97,8 @@ function setSquaresColor(){
     }
 }
 
-
+function hideSquares(){
+    for(var i = 3;i<squares.length;i++){
+        squares[i].style.backgroundColor = "#232323";
+    }
+}
